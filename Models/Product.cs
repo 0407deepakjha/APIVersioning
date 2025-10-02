a40e03342023124cb8c4d7656080ffd6222fdcca
Deepak Jha <jhadeepak0407@outlook.com>
2025-10-02 15:53:44 +0530

    DTOs | Models

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace APIVersioning.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Added in version 2
+        public double? Price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace APIVersioning
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add controllers with JSON options (disable camelCase)
+            builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Add API versioning services to DI
+            builder.Services.AddApiVersioning(options =>
+            {
+                // This allows the API to use a default version if the client does not specify one.
+                options.AssumeDefaultVersionWhenUnspecified = true;
+
+                // This sets the default version (here, 1.0).
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+
+                // This adds headers in the response informing clients about all the supported API versions.
+                options.ReportApiVersions = true;
+
+                // This tells the framework to use the query string parameter api-version for versioning
+                options.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
